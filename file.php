4334bcf92c23682c6fcf6e6057bd83696f5943de
Anuj Kumar <98866899+anujjsengar@users.noreply.github.com>
2023-01-30 21:21:17 +0530

    Add files via upload

---
+++ file.php
@@ -0,0 +1,10 @@
+<?php
+$file=fopen("AAGMAN.TXT","a");
+$q="NAG BIGADTA JAA RHA HE LADKIYO K PRAKOOP M AAKE";
+$read=fappend($file,$q);
+if(!$read){
+    echo "data added successfully";
+}
+else{
+    echo "unable to add data"
+}
